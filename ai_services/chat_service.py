e64a77bf3d4dd8f42daefb5409748d54190c5834
kiramaslennikova <Kirananana685@gmail.com>
2025-10-27 14:32:10 +0300

    Small update in chat_service.py

--- ai_services/chat_service.py
+++ ai_services/chat_service.py
@@ -12,7 +12,7 @@ texts = [
 vectorizer = TfidfVectorizer().fit(texts)
 matrix = vectorizer.transform(texts)
 
-def chatbot_reply(user_message: str = None, crop: str = None, disease: str = None):
+def generate_bot_reply(user_message: str = None, crop: str = None, disease: str = None):
     user_message = user_message.lower().strip() if user_message else ""
 
 
@@ -46,3 +46,4 @@ def chatbot_reply(user_message: str = None, crop: str = None, disease: str = Non
         + "\n".join(f"- {t}" for t in best_match['treatment'])
         + f"\n\n{best_match['chat_tip']}"
     )
+
